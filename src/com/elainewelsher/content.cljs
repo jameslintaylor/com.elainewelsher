f162effac9c2338abae52e9f26fa2eb9f6c2032f
jlt <jameslintaylor@gmail.com>
2021-01-24 08:56:27 -0500

    Add test content

--- src/com/elainewelsher/content.cljs
+++ src/com/elainewelsher/content.cljs
@@ -8,19 +8,19 @@
     :color       "pink"
     :description "Lorem, Toronto, 2020 <br><br> Ipsum dolor sit amet. <br><br> consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     :images      [{:thumbnail "images/lift/lift-thumb-axo.png"
-                   :full      "images/lift/lift-thumb-axo.png"
+                   :full      "images/lift/01-lift-little-portugal-site.png"
                    :title     "Axo"}
                   {:thumbnail "images/lift/lift-thumb-balcony.png"
-                   :full      "images/lift/lift-thumb-balcony.png"
+                   :full      "images/lift/02-lift-toronto-laneway-network-and-elderly-homes.png"
                    :title     "Balcony"}
                   {:thumbnail "images/lift/lift-thumb-front.png"
-                   :full      "images/lift/lift-thumb-front.png"
+                   :full      "images/lift/03-lift-little-portugal-neighbourhood.png"
                    :title     "Front"}
                   {:thumbnail "images/lift/lift-thumb-map.png"
-                   :full      "images/lift/lift-thumb-map.png"
+                   :full      "images/lift/04-lift-site-plan.png"
                    :title     "Map"}
                   {:thumbnail "images/lift/lift-thumb-section-1.png"
-                   :full      "images/lift/lift-thumb-section-1.png"
+                   :full      "images/lift/05-lift-laneway-dwelling.png"
                    :title     "Section 1"}
                   {:thumbnail "images/lift/lift-thumb-view-2.png"
                    :full      "images/lift/lift-thumb-view-2.png"
